07dad86330e183bb6ed57501acbf7d50a66706c2
Anjelica <ambrosio.anjelica@gmail.com>
2021-03-03 15:40:50 -0800

    finished challenge 5

--- src/components/BandDetails/BandDetails.js
+++ src/components/BandDetails/BandDetails.js
@@ -6,7 +6,8 @@ function BandDetails(props) {
   const { band_name, fans, formed, origin, split, style } = props
    return (
     <div className="BandDetails">
-      <h1>{band_name} {formed} - {split}</h1>
+      <h1>{band_name}</h1>
+      <div>Formed: {formed} - Split: {split}</div>
       <h3>Style: {style}</h3>
       <h3>From: {origin}</h3>
       <h3>Fans: {fans}</h3>
--- src/components/MetalMeta/MetalMeta.js
+++ src/components/MetalMeta/MetalMeta.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import data from '../../metal.json';
-import BandDetails from '../BandDetails/BandDetails'
+import BandDetails from '../BandDetails/BandDetails';
+import './MetalMeta.css';
 
 
 function MetalMeta() {
-
+  const bandcount = JSON.parse(JSON.stringify(data)).length
   const spaces = data.map(( { band_name, fans, formed, origin, split, style }, i) => {
     return (
       <BandDetails
@@ -20,7 +21,8 @@ function MetalMeta() {
   })
 
   return (
-    <div className="CastList">
+    <div className="MetalMeta">
+    <h2>Metal Bands 🤘: {bandcount}</h2>
       { spaces }
     </div>
   )
